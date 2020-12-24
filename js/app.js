e0193e0b25b185d2e55ee2d7acaa1010d591d761
Nathan Heck <36094454+MeatBot99@users.noreply.github.com>
2020-12-24 11:18:12 -0700

    First commit after making some progress

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 //App.js//
+/*TODO: Storage solution obviously. That is the main goal for this experiment. It
+        turns out that just working with key-value pairs without some string parsing will be
+        more involved than it seems on the surface.
+
+        Also a way to reorganize the cards by different stats and a data display of
+        a chart over time --data visualization-- would be a neat feature.
+*/
+let mainContainer = document.getElementById("main-container");
+let saveFile = localStorage;
+let createCard = document.getElementById("create-card");
+
+let createElements = function(elem, cls, content){
+  let newElem = document.createElement(elem);
+  newElem.classList.add(...cls);
+  newElem.innerText = content;
+  //newDiv.append(newElem);
+  return newElem;
+}
+
+
+let makeNewCard = function(){
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("new-div","card-template");
+  let titleInput = document.getElementById("title-input");
+  let titleText = titleInput.value;
+  newDiv.append(createElements("h2", ["cat"], titleText));
+  mainContainer.prepend(newDiv);
+};
+
+createCard.addEventListener("click", makeNewCard);
+console.log(saveFile, newCard);
--- js/data.js
+++ js/data.js
@@ -1,1 +1,22 @@
 //Data.js//
+let newCard = {
+    "heading": "Title of Card",
+    "date": "MM/DD/YYYY",
+    "iteration": "14",
+    "surveyer": "29",
+    "headhunter": "45",
+    "cultist": "78",
+    "nurse": "4",
+    "tile": "72",
+    "heritage": "Sacred Path",
+    "gold": "56.2B",
+    "ship": "1000/750",
+    "amber": "534",
+    "mentorship": "800.3B",
+    "forest": "13",
+    "chicken": "14",
+    "stone": "11",
+    "oni": "10",
+    "frog": "14",
+    "comments": "Check out this idea!"
+}
