31777b238b428d4973a92dcb0860c429af4474ae
Nathan Heck <36094454+MeatBot99@users.noreply.github.com>
2020-12-30 11:52:28 -0700

    Basic Structure improvement

--- js/app.js
+++ js/app.js
@@ -6,27 +6,47 @@
         Also a way to reorganize the cards by different stats and a data display of
         a chart over time --data visualization-- would be a neat feature.
 */
+let expDiv = document.getElementById("expDiv");
 let mainContainer = document.getElementById("main-container");
+let formStructure = document.getElementById("form-structure");
 let saveFile = localStorage;
 let createCard = document.getElementById("create-card");
+
 
 let createElements = function(elem, cls, content){
   let newElem = document.createElement(elem);
   newElem.classList.add(...cls);
   newElem.innerText = content;
-  //newDiv.append(newElem);
   return newElem;
 }
 
+let formBuild = function(){
+  for(thing in new Card){
+    let newItem = createElements("div", [`${thing}-value`,"container"],"");
+    formStructure.append(newItem);
+    let newLabel = createElements("label",[],thing);
+    //capitalize label content
+    newLabel.htmlFor = `${thing}-input`;
+    newItem.append(newLabel);
+    let newInput = createElements("input",[],"");
+    newInput.id = `${thing}-input`;
+    newInput.name = `${thing}-input`;
+    newItem.append(newInput);
+  }
+}
+
+formBuild();
 
 let makeNewCard = function(){
   let newDiv = document.createElement("div");
   newDiv.classList.add("new-div","card-template");
   let titleInput = document.getElementById("title-input");
   let titleText = titleInput.value;
-  newDiv.append(createElements("h2", ["cat"], titleText));
+
+  let newHeading = newDiv.append(createElements("h2", ["heading"], titleText));
   mainContainer.prepend(newDiv);
+  titleInput.value = "";
 };
 
 createCard.addEventListener("click", makeNewCard);
-console.log(saveFile, newCard);
+console.log(saveFile);
--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 //Data.js//
-let newCard = {
+/*let newCard = {
     "heading": "Title of Card",
     "date": "MM/DD/YYYY",
     "iteration": "14",
@@ -19,4 +19,26 @@ let newCard = {
     "oni": "10",
     "frog": "14",
     "comments": "Check out this idea!"
+}*/
+
+function Card(heading, date, iteration, surveyer, headhunter, cultist, nurse, tile, heritage,
+gold, ship, amber, mentorship, forest, chicken, stone, oni, frog, comments){
+  this.heading = heading;
+  this.date = date;
+  this.iteration = iteration;
+  this.surveyer = surveyer;
+  this.headhunter = headhunter;
+  this.cultist = cultist;
+  this.nurse = nurse;
+  this.tile = tile;
+  this.heritage = heritage;
+  this.gold = gold;
+  this.amber = amber;
+  this.mentorship = mentorship;
+  this.forest = forest;
+  this.chicken = chicken;
+  this.stone = stone;
+  this.oni = oni;
+  this.frog = frog;
+  this.comments = comments;
 }
